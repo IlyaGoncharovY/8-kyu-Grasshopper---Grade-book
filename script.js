6d2255d0e3d3bf9da60fdcdb54f951f1914ed66a
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-01-11 18:51:14 +0300

    test ok

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-hello
+function getGrade(s1, s2, s3) {
+    let sum = (s1 + s2 + s3) / 3;
+    return 90 <= sum && sum <= 100 ? "A"
+        : 80 <= sum && sum < 90 ? "B"
+            : 70 <= sum && sum < 80 ? "C"
+                : 60 <= sum && sum < 70 ? "D"
+                    : "F";
+}
